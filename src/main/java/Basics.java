4772e4542d597019262274956920a8fa15c2ea08
ericw <ericwang030@gmail.com>
2022-09-30 17:46:54 -0400

    Exercise 1

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -7,6 +7,8 @@
  * relevant readings for each task.
  */
 
+import java.util.ArrayList;
+
 /**
  *  1. In Java, no code can exist outside of a class. Unlike Python which uses
  *    functions, all code in Java uses methods.
@@ -43,7 +45,9 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
-
+        String s;
+        s = "Hello World!";
+        System.out.println(s);
 
 
         /* 4. In Python, we could declare variables using a simple assignment
@@ -62,7 +66,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -99,7 +103,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
-
+        for (int i = 10; i >= 0; i -= 1){
+            System.out.println("Current count: " + i);
+        }
 
     }
 
@@ -141,9 +147,11 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
+        for(int i = 0; i <= to_split.split(" ").length - 1; i += 1){
 
+            ret.append(to_split.split(" ")[i].charAt(0));
+        }
         // Fill in the rest of the body here
-
         return ret.toString();
     }
 
@@ -169,8 +177,16 @@ public class Basics {
          *                .length attribute (e.g. arr.length)
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
+         *
          */
-
+        if(arr.length == 1 | arr.length == 0){
+            return 0;
+        }
+        for(int i = 0; i <= (arr.length - 1); i += 1){
+            if (i %2 != 0){
+                current_sum += arr[i];
+            }
+        }
         return current_sum;
     }
 
